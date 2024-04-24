4ac950f3d537a90af6ff3a4f6c45910ee66ed2ce
Suraj Mandal <surajmandalcell@gmail.com>
2024-04-24 09:18:00 -0400

    - First look for WS port from env variable then default to 8080 - Prettier autoformat to singlequote

--- apps/ws/src/index.ts
+++ apps/ws/src/index.ts
@@ -1,23 +1,24 @@
 import { WebSocketServer } from 'ws';
 import { GameManager } from './GameManager';
-import url from "url";
+import url from 'url';
 import { extractUserId } from './auth';
 import { User } from './SocketManager';
 
-const wss = new WebSocketServer({ port: 8080 });
+const wss = new WebSocketServer({
+  port: parseInt(process.env.WS_PORT!) || 8080,
+});
 
 const gameManager = new GameManager();
 
 wss.on('connection', function connection(ws, req) {
-
   //@ts-ignore
   const token: string = url.parse(req.url, true).query.token;
   const userId = extractUserId(token);
   gameManager.addUser(new User(ws, userId));
 
-  ws.on("close", () => {
-    gameManager.removeUser(ws)
-  })
+  ws.on('close', () => {
+    gameManager.removeUser(ws);
+  });
 });
 
-console.log("done")
+console.log('done');
